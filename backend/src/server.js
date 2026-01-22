166fdc982ad019da7d7f008e45e0da1e1b20d139
PrasannaGodiyal3903 <godiyalprasanna16@gmail.com>
2026-01-22 11:48:27 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@
 import upstash from "./config/upstash.js";
 import express from "express"
 import cors from "cors"
+import path from "path"
 import notesRoute from "./routes/notesRoute.js";
 import{connectDB} from "./config/db.js"
 import dotenv from "dotenv"
@@ -9,16 +10,25 @@ import rateLimiter from "./middleware/rateLimiter.js";
 dotenv.config();
 
 const app=express()
+const __dirname=path.resolve()
+if(process.env.NODE_ENV!=="production"){
 app.use(
     cors({
         origin:"http://localhost:5173",
     })
 )
+}
 app.use(express.json()); //gets access for req.body()
 app.use(rateLimiter)
 
 const PORT=process.env.PORT || 5001
 app.use("/api/notes",notesRoute);
+if(process.env.NODE_ENV==="production"){
+app.use(express.static(path.join(__dirname,"../frontend/dist")))
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+})
+}
 // app.get("/api/notes",(req,res)=>{
 //     res.status(200).send("hello you got nothing here");  
 // });
